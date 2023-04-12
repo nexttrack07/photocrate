fe950fb66a4990228895ecf0f929b5b56ac3b83a
Faadhil Hakim <faadhil@nexttrack.biz>
2023-04-12 18:46:37 -0500

    upgrade pip

--- render/build.sh
+++ render/build.sh
@@ -4,6 +4,7 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+pip install --upgrade pip
 pip install -r requirements/production.txt
 # python /app/manage.py collectstatic --noinput
 python /app/manage.py migrate
