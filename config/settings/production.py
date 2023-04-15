f4c7a3f2a7b39c3e0aeccf99c638fefd331e7941
Faadhil Hakim <faadhil@nexttrack.biz>
2023-04-15 01:46:45 -0500

    remove acl from settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -81,7 +81,6 @@ AWS_S3_REGION_NAME = env("DJANGO_AWS_S3_REGION_NAME", default=None)
 AWS_S3_CUSTOM_DOMAIN = env("DJANGO_AWS_S3_CUSTOM_DOMAIN", default=None)
 aws_s3_domain = AWS_S3_CUSTOM_DOMAIN or f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
-AWS_DEFAULT_ACL = None
 # STATIC
 # ------------------------
 STATICFILES_STORAGE = "photocrate.utils.storages.StaticRootS3Boto3Storage"
