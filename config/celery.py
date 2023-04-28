dd34bbff652df9ee88b631c29cfcaa4495ff91ab
Faadhil Hakim <faadhil@nexttrack.biz>
2023-04-27 20:40:44 -0500

    added celery.py

---
+++ config/celery.py
@@ -0,0 +1,20 @@
+import os
+
+from celery import Celery
+from django.conf import settings
+
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project_name.settings")
+
+app = Celery("your_project_name")
+
+# Use a string here to use the 'redis://' URL.
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f"Request: {self.request!r}")
